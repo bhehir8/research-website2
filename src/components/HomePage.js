4029c266fe3f5b92ce715e0e83057ee992c6c450
mauradriscoll <51652231+mauradriscoll@users.noreply.github.com>
2020-07-30 14:33:23 -0400

    feat: 🎸 footer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,10 +5,13 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/Form';
 import logo from '../Northeastern_logo.svg.png';
+import brendan from '../Brendan_web.png';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../custom.scss';
+import 'mdbreact/dist/css/mdb.css' 
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 export default class HomePage extends React.Component {
     render() {
@@ -50,5 +53,46 @@ Replacing the Workforce</strong> */}
         Replacing the Workforce</h1>
   </Container>
 </Jumbotron>
+<MDBFooter color="red" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+      
+        <MDBRow>
+        <MDBCol md="2">
+      <img
+        src={brendan}
+        width="100"
+        height="150"
+        className="d-inline-block align-top"
+      />
+            
+          </MDBCol>
+          <MDBCol >
+            <h5 className="title">Brendan Hehir</h5>
+            <p>
+              Aerobic Machine
+            </p>
+            <p>
+              Always Feeling Dangerous
+            </p>
+            
+          </MDBCol>
+          <MDBCol >
+            <h5 className="title">Contact</h5>
+            <ul>
+              <li className="list-unstyled">
+              <p>
+              Any Place, Any Time           
+                </p>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright
+        </MDBContainer>
+      </div>
+    </MDBFooter>
     </div>
       )}}
