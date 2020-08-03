395c0a3bb933fe3a9c871c2b8414ef3b3f760b68
ASM-NEXX Hehir <brendan.x.hehir@nexx.com>
2020-08-02 22:14:13 -0400

    rev03

--- src/components/Minorities.js
+++ src/components/Minorities.js
@@ -19,7 +19,12 @@ Replacing the Workforce</strong> */}
 </h1>
   </Container>
 </Jumbotron>
-<p> Hello Mother </p>
+<p> The root cause of the unfair disadvantage held by black and hispanic populations goes back centuries, however, the modern day socio-economic disadvantage can be accredited to the redlining practices of the 1930s. Redlining essentially denied minority communities services and funding from local and federal governments. The effects of these practices are still felt today as poverty is far more concentrated in black and Hispanic communities as opposed to white communities. These negative effects are profoundly demonstrated in the inequality of public schools. Because schools are largely funded by local property taxes, the schools in black and hispanic communities receive far less funding than those is white communities.  It’s estimated that predominantly black and Hispanic school districts receive $23 million less than the predominantly white school district despite serving equal amounts of students.
+The unequal funding of the schools at the elementary and high school levels are then reflected at the collegiate level. The table below describes this well:
+
+The table indicates that white and Asian students make up a disproportionately high percentage of the more selective school, with black and Hispanic students being underrepresented at these universities. Thus, the black and Hispanic populations are overrepresented in the less prestigious schools. The image below graphically illustrates the message above.
+
+ </p>
 
 
     </div>
--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -18,7 +18,11 @@ Replacing the Workforce</strong> */}
     <h1>Suggestions</h1>
   </Container>
 </Jumbotron>
-<p> Hey Pickle </p>
+<p>
+The biggest suggestion I have to mitigate the negative effects of job polarization, is to fix the educational system. All public schools, regardless of nearby property value, should be given equal funding. This would require restructuring the way schools receive funding from the government. As opposed to having property taxes only serve the scool districts within its borders, the property taxes ought to be pooled across the state. From this large pool of money, each school should be funded accordingly based off the number of students they serve. This would even the educational playing field for children across all communities. 
+Furthermore, colleges must play an active role in recruiting, and maintaining students from minority populations. This can be done by creating scholarships so that these students can attend the top tier college they otherwise could not have afforded. Furthermore, mentorship programs can be created to help keep minority students on pace to graduate.
+Lastly, the educational system should update its curriculum to better prepare students for the new future of work. This would require giving more focus to coding, analytics, as well as interpersonal skills. However, it is important to note that classes in art, history, and literature and so on should not be undermined. These fields are inherently immune to automation and add cultural value to society. But, if we raise the standard of the typical education level, the higher skilled job market can become saturated, thus lowering the wages and depolarizing the market.
+ </p>
 
     </div>
       )}}
